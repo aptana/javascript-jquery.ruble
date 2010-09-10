7e357b82f9886f9a84b8b5874f94744c5db2306e
sgtcoolguy <cwilliams@aptana.com>
2010-09-10 15:11:58 -0700

    convert to block invocation

--- commands/documentation_for_word.rb
+++ commands/documentation_for_word.rb
@@ -5,50 +5,46 @@ command 'Documentation for Selection (jQuery)' do |cmd|
   cmd.scope = 'source.js'
   cmd.output = :show_as_html
   cmd.input = :selection, :word
-  cmd.invoke =<<-EOF
-#!/usr/bin/env ruby
-
-require 'rexml/document'
-require 'tempfile'
-include REXML
-file = File.open ENV['TM_BUNDLE_SUPPORT'] + '/jquery-docs-xml-142.xml'
-
-doc = Document.new(file)
-root = doc.root
-
-sel_text = ENV['TM_SELECTED_TEXT']
-
-if sel_text.nil?
-  cur_line = ENV['TM_CURRENT_LINE']
-  cur_word = ENV['TM_CURRENT_WORD']
-  matcher = cur_line.match(/(jQuery|\$)(\.#{cur_word})/)
-  lookup_word = matcher ? 'jQuery' + matcher[2] : cur_word
-else
-  lookup_word = sel_text
-end
-lookup_word = lookup_word.sub('$','jQuery')
-
-entries = ''
-root.elements.each("//entry[@name='#{lookup_word}']") { |e| entries += e.to_s }
-
-stylefile = ENV['TM_BUNDLE_SUPPORT'] + '/docs-142.xsl'
-
-tf = Tempfile.new('docs')
-tf.puts('<?xml version="1.0" encoding="UTF-8"?>' + "\n")
-tf.puts('<docs>' + "\n")
-tf.puts entries
-tf.puts('</docs>' + "\n")
-tf.close
-xmlfile = tf.path
-
-output = `xsltproc "#{stylefile}" "#{xmlfile}"`
-
-if output.length < 100
-  url = "https://developer.mozilla.org/en?search=title:" + lookup_word
-  puts "<meta http-equiv='Refresh' content='0;URL=#{url}'>"
-else
-  puts output
-end
-
-EOF
+  cmd.invoke do
+    require 'rexml/document'
+    require 'tempfile'
+    include REXML
+    file = File.open(File.join(ENV['TM_BUNDLE_SUPPORT'], 'jquery-docs-xml-142.xml'))
+    
+    doc = Document.new(file)
+    root = doc.root
+    
+    sel_text = ENV['TM_SELECTED_TEXT']
+    if sel_text.nil?
+      cur_line = ENV['TM_CURRENT_LINE']
+      cur_word = ENV['TM_CURRENT_WORD']
+      matcher = cur_line.match(/(jQuery|\$)(\.#{cur_word})/)
+      lookup_word = matcher ? 'jQuery' + matcher[2] : cur_word
+    else
+      lookup_word = sel_text
+    end
+    lookup_word = lookup_word.sub('$','jQuery')
+    
+    entries = ''
+    root.elements.each("//entry[@name='#{lookup_word}']") { |e| entries += e.to_s }
+    
+    stylefile = File.join(ENV['TM_BUNDLE_SUPPORT'], 'docs-142.xsl')
+    
+    tf = Tempfile.new('docs')
+    tf.puts('<?xml version="1.0" encoding="UTF-8"?>' + "\n")
+    tf.puts('<docs>' + "\n")
+    tf.puts entries
+    tf.puts('</docs>' + "\n")
+    tf.close
+    xmlfile = tf.path
+    # FIXME This definitely won't fly on Windows!
+    output = `xsltproc "#{stylefile}" "#{xmlfile}"`
+    
+    if output.length < 100
+      url = "https://developer.mozilla.org/en?search=title:" + lookup_word
+      "<meta http-equiv='Refresh' content='0;URL=#{url}'>"
+    else
+      output
+    end
+  end
 end
